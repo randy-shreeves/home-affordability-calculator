82bd97998ffd29ab1566a7df492acbc71865602a
randy <shreevesrandy@gmail.com>
2026-05-26 15:08:07 -0400

    Initial project setup

---
+++ src/main/java/com/randyshreeves/home_affordability_calculator/HomeAffordabilityCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.randyshreeves.home_affordability_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAffordabilityCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAffordabilityCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randyshreeves/home_affordability_calculator/HomeAffordabilityCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randyshreeves.home_affordability_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAffordabilityCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
